2610c13229f69308f5a935e94db981e3d33d4d05
John Gardner <john@arconixpc.com>
2012-12-09 00:48:57 -0500

    consolidate functions

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Register the necessary javascript, which can be overriden by creating your own file and
  * placing it in the root of your theme's folder
@@ -7,7 +6,7 @@
  * @since 0.9
  * @version 1.1.0
  */
-function register_javascript() {
+function acs_load_scripts() {
     /* Provide script registration args so they can be filtered if necessary */
     $script_args = apply_filters( 'arconix_jquerytools_reg', array(
         'url' => 'http://cdn.jquerytools.org/1.2.7/tiny/jquery.tools.min.js',
@@ -17,30 +16,19 @@ function register_javascript() {
 
     wp_register_script( 'jquery-tools', $script_args['url'], array( $script_args['dep'] ), $script_args['ver'], true );
 
-    if( file_exists( get_stylesheet_directory() . '/arconix-shortcodes.js' ) ) {
+    if( file_exists( get_stylesheet_directory() . '/arconix-shortcodes.js' ) )
         wp_register_script( 'arconix-shortcodes-js', get_stylesheet_directory_uri() . '/arconix-shortcodes.js', array( 'jquery-tools' ), ACS_VERSION, true );
-    } elseif( file_exists( get_template_directory() . '/arconix-shortcodes.js' ) ) {
+    elseif( file_exists( get_template_directory() . '/arconix-shortcodes.js' ) )
         wp_register_script( 'arconix-shortcodes-js', get_template_directory_uri() . '/arconix-shortcodes.js', array( 'jquery-tools' ), ACS_VERSION, true );
-    } else {
+    else
         wp_register_script( 'arconix-shortcodes-js', ACS_INCLUDES_URL . 'shortcodes.js', array( 'jquery-tools' ), ACS_VERSION, true );
-    }
-}
 
-/**
- * Load the necessary css, which can be overriden by creating your own file and placing it in
- * the root of your theme's folder
- *
- * @since 0.9
- * @version 1.1.0
- */
-function enqueue_css() {
-    if( file_exists( get_stylesheet_directory() . '/arconix-shortcodes.css' ) ) {
+    if( file_exists( get_stylesheet_directory() . '/arconix-shortcodes.css' ) )
         wp_enqueue_style( 'arconix-shortcodes', get_stylesheet_directory_uri() . '/arconix-shortcodes.css', array(), ACS_VERSION );
-    } elseif( file_exists( get_template_directory() . '/arconix-shortcodes.css' ) ) {
+    elseif( file_exists( get_template_directory() . '/arconix-shortcodes.css' ) )
         wp_enqueue_style( 'arconix-shortcodes', get_template_directory_uri() . '/arconix-shortcodes.css', array(), ACS_VERSION );
-    } else {
+    else
         wp_enqueue_style( 'arconix-shortcodes', ACS_INCLUDES_URL . 'shortcodes.css', array(), ACS_VERSION );
-    }
 }
 
 /**
