33a9b7b4dc4b858c9551c6030f9a9400f87a369d
bhavik <bhavik@bhavik>
2018-08-13 18:25:58 +0530

    Commented Welcome page
    We have commented the welcome page.

--- includes/arconix-shortcodes-all-component.php
+++ includes/arconix-shortcodes-all-component.php
@@ -57,11 +57,11 @@ if ( ! class_exists( 'Arconix_Shortcodes_Component' ) ) {
                 $shortcodes_deativate = new Shortcodes_TS_deactivate;
                 $shortcodes_deativate->init ( $shortcodes_file_name, $shortcodes_plugin_name );
                 
-                $user = wp_get_current_user();
+                // $user = wp_get_current_user();
                 
-                if ( in_array( 'administrator', (array) $user->roles ) ) {
-                    new Shortcodes_TS_Welcome ( $shortcodes_plugin_name, $shortcodes_plugin_prefix, $shortcodes_locale, $shortcodes_plugin_folder_name, $shortcodes_plugin_dir_name, $shortcodes_get_previous_version );
-                }
+                // if ( in_array( 'administrator', (array) $user->roles ) ) {
+                //     new Shortcodes_TS_Welcome ( $shortcodes_plugin_name, $shortcodes_plugin_prefix, $shortcodes_locale, $shortcodes_plugin_folder_name, $shortcodes_plugin_dir_name, $shortcodes_get_previous_version );
+                // }
 
                 $ts_pro_shortcodes = self::shortcodes_get_faq ();
                 new Shortcodes_TS_Faq_Support( $shortcodes_plugin_name, $shortcodes_plugin_prefix, $shortcodes_plugins_page, $shortcodes_locale, $shortcodes_plugin_folder_name, $shortcodes_plugin_slug, $ts_pro_shortcodes, '', $shortcodes_file_name );
